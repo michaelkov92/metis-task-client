2cd8fa31c063fa828790f4ee304436c7b2a3d21b
Michael Kov <53880007+michaelkov92@users.noreply.github.com>
2024-04-01 16:30:38 +0300

    added toast to when no initial connection

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
 import { fetchAvailableFiles } from './services/http';
 import { onMounted } from 'vue';
 import { useWordSetsStore } from './stores/wordSetsStore';
+import Toast from 'primevue/toast';
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
 const wordSetsStore = useWordSetsStore();
 onMounted(async () => {
   const fileNames = await fetchAvailableFiles()
-  console.log(fileNames);
+  if (!fileNames) {
+    toast.add({ severity: 'warn', summary: 'Connection error', detail: 'Failed To fetch word files', life: 3000 });
+  }
   wordSetsStore.setFilesList(fileNames);
 })
 </script>
 
 <template>
   <div class="app-container">
+    <Toast />
     <Header class="header"></Header>
     <Home class="home"></Home>
   </div>
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'primeicons/primeicons.css';
+import ToastService from 'primevue/toastservice';
 import Tooltip from 'primevue/tooltip';
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
-app.use(PrimeVue);
+app.use(PrimeVue).use(ToastService);
 app.directive('tooltip', Tooltip);
 app.mount('#app');
